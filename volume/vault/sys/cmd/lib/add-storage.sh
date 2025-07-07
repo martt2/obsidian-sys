626d93c0b7ad88809e2f92f2c53fa38b95b1cc0c
mhco1 <mhco.etc@tuta.com>
2025-07-07 17:16:30 -0300

    alter add-storage.sh

--- volume/vault/sys/cmd/lib/add-storage.sh
+++ volume/vault/sys/cmd/lib/add-storage.sh
@@ -4,18 +4,7 @@ UUID=$(uuidgen)
 USER=$1
 NAME=$2
 TAG="$(echo $3 | sed 's|\(^.\)|#\1|g' | sed 's|#|\n - |g')"
-
-BTN=$(cat <<-EOF
-|-
-  \`BUTTON[btn-work, btn-remove]\`
-
-  \`\`\`meta-bind-embed
-  [[sys/model/meta-bind-embed/btn-view]]
-  \`\`\`
-
-EOF
-
-)
+NOTE="[open note](view/data/$UUID.md)"
 
 VIEW=$(cat <<-EOF
 ---
@@ -23,8 +12,20 @@ id: $UUID
 user: $USER
 name: $NAME
 tag: $TAG
-btn: $BTN
+note: "$NOTE"
 ---
+
+# $NAME
+
+---
+
+\`\`\`meta-bind-embed
+[[sys/model/meta-bind-embed/btn-control]]
+\`\`\`
+
+\`\`\`meta-bind-embed
+[[sys/model/meta-bind-embed/btn-goto]]
+\`\`\`
 
 EOF
 
