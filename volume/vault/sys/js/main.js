a2026bf72f5281bb0ecbe02ee4f97877c3ab8523
mhco1 <mhco.etc@tuta.com>
2025-07-09 10:49:36 -0300

    add main.js

--- volume/vault/sys/js/main.js
+++ volume/vault/sys/js/main.js
@@ -1,3 +1,23 @@
 class main {
-    
+    flexbox(dv, ...arr) {
+        dv.container.className += "sys-flexbox-container"
+        const div = dv.el("div", "", {
+            container: dv.container
+        })
+        const ul = dv.el("ul", "", {
+            container: div,
+            cls: "sys-flexbox-ul"
+        })
+
+        ul.innerText = "";
+
+        arr.forEach(el => {
+            return dv.el("li", el, {
+                container: ul,
+                cls: "sys-flexbox-li"
+            })
+        })
+
+        return dv.container
+    }
 }
